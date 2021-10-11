2fd4807fa6d9c005614e7a6791f347dd327e2953
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2021-10-11 17:58:59 -0400

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+-- 1. List all artists for each record label sorted by artist name. 
+select
+	ar.name "Artist Name",
+	r.name "Record Label Name"  
+from
+	record_label r join artist ar on r.id = ar.record_label_id
+order by
+	ar.name asc
+;	
