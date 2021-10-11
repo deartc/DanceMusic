d40d992111bd05e442ba3ef25bb3402a9ba9cb64
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2021-10-11 17:58:32 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+--  List the number of songs per artist in descending order
+
+select
+    ar.name "Artist Name",
+    count(*) "Number of Songs"
+from
+    song s join album  al on s.album_id = al.id
+    	   join artist ar on al.artist_id = ar.id
+group by
+    ar.name
+order by
+    count(*) desc
+;   
