3a0e02df50d2b50bccaaf24f59d7d8b9bd96e97b
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2021-11-08 07:30:26 -0500

    Delete Query2.sql

--- Query2.sql
+++
@@ -1,9 +0,0 @@
---  List all artists for each record label sorted by artist name. 
-select
-	ar.name "Artist Name",
-	r.name "Record Label Name"  
-from
-	record_label r join artist ar on r.id = ar.record_label_id
-order by
-	ar.name asc
-;	
