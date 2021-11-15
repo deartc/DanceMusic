0ef147652b9b90753c90f7f9f5f40be16466082a
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2021-11-14 23:54:14 -0500

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -26,7 +26,23 @@ order by
 	SELECT artist
 	FROM album JOIN track
 	ON (album.asin = track.album)
-	WHERE song = 'Joy'
+	WHERE song = 'Joy'--Which artist or artists have recorded the most number of songs?
+
+select
+   ar.name "Artist Name",
+   count(*) "Number of Songs"
+from
+    song s join album al on s.album_id = al.id
+    	   join artist ar on al.artist_id = ar.id 
+group by
+   ar.name
+order by
+   count(*) desc
+limit 1
+	
+	
+	
+	
 ;   
 
 
